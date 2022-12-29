53cb5cd8e2712921acec278a83819db121174b73
kowqo <kowqo@yandex.ru>
2022-12-29 23:55:47 +0300

    add user

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get("/test", function () {
+
+    return User::get();
+});
+
+Route::get("hello", function () {
+    return "<p>Hello World!</p>";
+});
+Route::get("aqq", function () {
+    return "aaa";
+});
+
